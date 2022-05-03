1675db10b80683415fb14dfc4566daa0cbbbd700
Nafiu Mubashir <mubashirnafiu@gmail.com>
2022-05-03 11:10:50 +0100

    Started adding the features of call

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let addVoucher = JSON.parse(localStorage.getItem("allVouchers"));
 let voucher;
+
 function generate() {
     let numOfCard = num.value;
     let network = networks.value;
@@ -12,24 +13,26 @@ function generate() {
             cardType: network,
             cardAmount: amount,
             cardNo: card,
+            cardStatus: "notUsed"
         }
         if (addVoucher) {
             addVoucher = [...addVoucher, voucher];
             localStorage.setItem("allVouchers", JSON.stringify(addVoucher));
-        }
-        else {
+        } else {
             localStorage.setItem("allVouchers", JSON.stringify([voucher]));
         }
     }
 }
+
 function display() {
     let all = JSON.parse(localStorage.getItem("allVouchers"));
     console.log((all[0].cardNo));
     for (let i = 0; i < all.length; i++) {
         const element = all[i];
-        output.innerHTML += `<div style="background-color:white;height:100px"> <span style="font-size:16px;font-weight:bolder;">${element.cardType}</span><span style="color:green;font-size:15px;font-weight:bolder;">unused</span> <span style="font-size:16px;font-weight:bolder;">${element.cardAmount}</span><hr><span style="text-align:center;color:green;font-size:16px;font-weight:bolder;margin-bottom:50px;">${element.cardNo}</span></div> </br>`;
+        output.innerHTML += `<div class="bg-white" style="height:100px"> <span style="font-size:16px;font-weight:bolder;">${element.cardType}</span><span style="color:green;font-size:15px;font-weight:bolder;">unused</span> <span style="font-size:16px;font-weight:bolder;">${element.cardAmount}</span><hr><span style="text-align:center;color:green;font-size:16px;font-weight:bolder;margin-bottom:50px;">${element.cardNo}</span></div> </br>`;
     }
 }
+
 function run() {
     let defaultDate = new Date;
     let defaultHour = defaultDate.getHours();
@@ -45,8 +48,7 @@ function run() {
         // currentSec.innerText = defaultSeconds;
         currentHours.innerText = defaultHour;
         currentMinutes.innerHTML = defaultMinute;
-    }
-    else {
+    } else {
         currentHr.innerText = defaultHour;
         currentMin.innerHTML = defaultMinute + " " + `<sub style="font-size:10px;margin-left:-8px;">PM</sub>`;
         // currentSec.innerText = defaultSeconds;
@@ -61,29 +63,35 @@ function unlock() {
     lockScreen.hidden = true;
     homeScreen.hidden = false;
 }
+
 function menue() {
     homeScreen.hidden = true;
     menuScreen.hidden = false;
 }
+
 function calLog() {
     homeScreen.hidden = true;
     menuScreen.hidden = true;
     callScreen.hidden = false;
 }
+
 function Voucher() {
     menuScreen.hidden = true;
     voucherScreen.hidden = false;
 }
+
 function inserts(e) {
     keyScreen.value += e;
-    console.log(keyScreen.value);
+    // console.log(keyScreen.value);
 }
+
 function clearIt() {
     var input = keyScreen.value;
     var b = input.substring(0, input.length - 1);
     var c = input.substring(0, input.length - 1);
     keyScreen.value = c;
 }
+
 function back() {
     homeScreen.hidden = true;
     menuScreen.hidden = false;
@@ -92,19 +100,23 @@ function back() {
     gameScreen.hidden = true;
     alarmScreen.hidden = true;
 }
+
 function home() {
     homeScreen.hidden = false;
     menuScreen.hidden = true;
     callScreen.hidden = true;
     voucherScreen.hidden = true;
     gameScreen.hidden = true;
-    alarmScreen.hidden =true;''
+    alarmScreen.hidden = true;
+    ''
 }
+
 function alarmSet() {
     homeScreen.hidden = true;
     menuScreen.hidden = true;
     alarmScreen.hidden = false;
 }
+
 function game() {
     menuScreen.hidden = true;
     gameScreen.hidden = false;
@@ -115,6 +127,7 @@ let min;
 let sec;
 let sound = new Audio();
 sound.src = "audio/alarm.mp3";
+
 function myCount() {
     let defaultDate = new Date;
     let defaultHour = defaultDate.getHours();
@@ -130,17 +143,20 @@ function myCount() {
         sound.loop = true;
     }
 }
+
 function setAlarm() {
     hr = hours.value;
     min = minutes.value;
     sec = seconds.value;
     console.log(hr + min + sec);
 }
+
 function stopAlarm() {
     alert("What is the answer to this mathematics arithmetic? 19 + 12");
     alarminp.hidden = false;
     sub.hidden = false;
 }
+
 function submit() {
     let answer = 31;
     let input = alarminp.value;
@@ -149,8 +165,7 @@ function submit() {
         sound.loop = false;
         inp.hidden = true;
         sub.hidden = true;
-    }
-    else {
+    } else {
         alert("Try again");
     }
 }
@@ -162,6 +177,7 @@ let lose = "oops! You lose ";
 let draw = " You draw with "
 let sounds = new Audio();
 sounds.src = "audio/Right_Said_Fred_-_Stand_Up_For_The_Champions__[NaijaGreen.Com]_.mp3";
+
 function startGame() {
     let firstPlayer = first.value;
     let secondPlayer = second.value;
@@ -179,8 +195,7 @@ function startGame() {
         seven.disabled = false;
         eight.disabled = false;
         nine.disabled = false;
-    }
-    else if (start.innerText == "End" && firstPlayer && secondPlayer) {
+    } else if (start.innerText == "End" && firstPlayer && secondPlayer) {
         start.innerText = "Start";
         outputs.innerHTML = "";
         first.value = "";
@@ -209,8 +224,7 @@ function startGame() {
         seven.innerHTML = "";
         eight.innerHTML = "";
         nine.innerHTML = "";
-    }
-    else {
+    } else {
         start.innerText = "Start";
         outputs.innerHTML = "Please enter your name..."
         one.innerHTML = "";
@@ -232,8 +246,7 @@ function play(e) {
         e.target.innerText = "X";
         check = false;
         outputs.innerHTML = " ";
-    }
-    else {
+    } else {
         e.target.innerText = "O";
         check = true;
     }
@@ -251,8 +264,7 @@ function play(e) {
         seven.disabled = true;
         eight.disabled = true;
         nine.disabled = true;
-    }
-    else if (one.innerHTML == "O" && two.innerHTML == "O" && three.innerHTML == "O" || four.innerHTML == "O" && five.innerHTML == "O" && six.innerHTML == "O" || seven.innerHTML == "O" && eight.innerHTML == "O" && nine.innerHTML == "O" || one.innerHTML == "O" && four.innerHTML == "O" && seven.innerHTML == "O" || two.innerHTML == "O" && five.innerHTML == "O" && eight.innerHTML == "O" || three.innerHTML == "O" && six.innerHTML == "O" && nine.innerHTML == "O" || one.innerHTML == "O" && five.innerHTML == "O" && nine.innerHTML == "O" || three.innerHTML == "O" && five.innerHTML == "O" && seven.innerHTML == "O") {
+    } else if (one.innerHTML == "O" && two.innerHTML == "O" && three.innerHTML == "O" || four.innerHTML == "O" && five.innerHTML == "O" && six.innerHTML == "O" || seven.innerHTML == "O" && eight.innerHTML == "O" && nine.innerHTML == "O" || one.innerHTML == "O" && four.innerHTML == "O" && seven.innerHTML == "O" || two.innerHTML == "O" && five.innerHTML == "O" && eight.innerHTML == "O" || three.innerHTML == "O" && six.innerHTML == "O" && nine.innerHTML == "O" || one.innerHTML == "O" && five.innerHTML == "O" && nine.innerHTML == "O" || three.innerHTML == "O" && five.innerHTML == "O" && seven.innerHTML == "O") {
         user2.innerText++;
         outputs.innerText = win + second.value + ", but " + lose + first.value;
         sounds.play();
@@ -266,8 +278,7 @@ function play(e) {
         seven.disabled = true;
         eight.disabled = true;
         nine.disabled = true;
-    }
-    else if ((one.innerHTML == "O" || one.innerHTML == "X") && (two.innerHTML == "O" || two.innerHTML == "X") && (three.innerHTML == "O" || three.innerHTML == "X") && (four.innerHTML == "O" || four.innerHTML == "X") && (five.innerHTML == "O" || five.innerHTML == "X") && (six.innerHTML == "O" || six.innerHTML == "X") && (seven.innerHTML == "O" || seven.innerHTML == "X") && (eight.innerHTML == "O" || eight.innerHTML == "X") && (nine.innerHTML == "O" || nine.innerHTML == "X")) {
+    } else if ((one.innerHTML == "O" || one.innerHTML == "X") && (two.innerHTML == "O" || two.innerHTML == "X") && (three.innerHTML == "O" || three.innerHTML == "X") && (four.innerHTML == "O" || four.innerHTML == "X") && (five.innerHTML == "O" || five.innerHTML == "X") && (six.innerHTML == "O" || six.innerHTML == "X") && (seven.innerHTML == "O" || seven.innerHTML == "X") && (eight.innerHTML == "O" || eight.innerHTML == "X") && (nine.innerHTML == "O" || nine.innerHTML == "X")) {
         outputs.innerText = "oops! " + first.value + draw + second.value;
     }
 }
@@ -299,36 +310,68 @@ function replay() {
 function insert(num) {
     displayOutput.value += num;
 }
+
 function equall() {
     displayOutput.value = eval(displayOutput.value);
 }
+
 function clears() {
     displayOutput.value = " ";
 }
+
 function del() {
     var a = displayOutput.value;
     var b = a.substring(0, a.length - 1);
     var c = a.substring(0, a.length - 1);
     displayOutput.value = c
 }
-let myLocalStore = JSON.parse(localStorage.getItem("allVouchers"));
-function callOne() {
-    let loadCard = keyScreen.value.includes("*555*");
-    let myData = myLocalStore.find((val, i) => val.numOfCard)
-    console.log(myData);
-    // if (loadCard + myData == keyScreen.value + "#") {
-
-    //     acctBalance.hidden = false;
-    // }
+let myAirtimes = JSON.parse(localStorage.getItem("allVouchers"));
+let balance = {
+    mtnBal: 0,
+    gloBal: 0
 }
-// console.log(myLocalStore);
-// function login() {
-//     let myArray = myLocalStore.find((val, i) => val.fName == username.value && val.pwrd == logPwrd.value)
-//     if (!myArray) {
-//         alert('user does not exist');
-//     }
-//     else {
-//         location.href = 'index.html';
-//     }
-//     console.log(myArray);
-// }
+// localStorage.setItem("acctBals", JSON.stringify(balance))
+function callOne() {
+    let loadCard = keyScreen.value.slice(0, 5) == "*555*";
+    let myData = myAirtimes.find((val, i) => val.cardNo == keyScreen.value.slice(5, 22))
+    if (keyScreen.value.length == 23) {
+        for (let i = 0; i < myAirtimes.length; i++) {
+            cardCode = myAirtimes[i].cardNo;
+            if ((loadCard) && (myData) && (keyScreen.value.slice(22, 23) == "#") && (myData.cardType == "MTN") && (myData.cardStatus == "notUsed")) {
+                console.log(keyScreen.value.slice(5, 22));
+                myData.cardStatus = "Used";
+                console.log(myData.cardStatus);
+                let bal = +myData.cardAmount;
+                let balances = JSON.parse(localStorage.getItem("acctBals"));
+                balances.mtnBal += bal;
+                acctBal.innerHTML = `Recharge successful. Your account has been credited with MTN ${myAirtimes.cardAmount}. Your new account bal :₦${balances.mtnBal.toFixed(2)} <br><br> <button class="btn btn-primary" onclick="hide()">Ok</button>`;
+                acctBal.hidden = false;
+                localStorage.setItem("acctBals", JSON.stringify(balances));
+                localStorage.setItem("allVouchers", JSON.stringify(myAirtimes));
+            } else if ((loadCard) && (myData) && (keyScreen.value.slice(22, 23) == "#") && (myData.cardType == "MTN") && (myData.cardStatus == "Used")) {
+                acctBal.innerHTML = `This card has been loaded by a customer. Thanks!!! <br><br> <button class="btn btn-primary" onclick="hide()">Ok</button>`;
+                acctBal.hidden = false;
+            }
+        }
+    } else if (keyScreen.value.slice(0, 5) == "*556#") {
+        acctBal.hidden = false;
+        let displayBal = JSON.parse(localStorage.getItem("acctBals"));
+        let showBal = displayBal.mtnBal;
+        acctBal.innerHTML = `Your MTN account balance is ₦${showBal.toFixed(2)} <br><br> <button class="btn btn-primary" onclick="hide()">Ok</button>`;
+    } else if ((keyScreen.value != "") && (keyScreen.value.length == 11) && ((keyScreen.value.slice(0, 3) == "070") || (keyScreen.value.slice(0, 3) == "080") || (keyScreen.value.slice(0, 3) == "090") || (keyScreen.value.slice(0, 3) == "081"))) {
+        let displayBal = JSON.parse(localStorage.getItem("acctBals"));
+        if (displayBal.mtnBal <= 0.50) {
+            acctBal.innerHTML = `Your account balance is too low for this call. You can borrow airtime or call back later. <br><br> <button class="btn btn-primary" onclick="hide()">Ok</button>`;
+            acctBal.hidden = false;
+            
+        }
+        else if (displayBal.mtnBal != 0) {
+            network.innerText = `MTN...`
+        }
+    }
+}
+
+hide = () => {
+    acctBal.hidden = true;
+    keyScreen.value = "";
+}
